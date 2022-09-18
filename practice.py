178fbc7fe95b50e9749adcf9717dcc8536e4ee47
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-09-18 18:11:55 +0500

    Lecture 2
    Python Intro.

---
+++ practice.py
@@ -0,0 +1,39 @@
+# my_list = ['This is val1', 2, 3, ['a', 4]]
+# print(my_list[3][1])
+# print(len(my_list))
+# print(my_list[:-1])
+# my_list[0] = 'This is new val'
+# print(my_list)
+# print(my_list + ['new item'])
+# print(my_list)
+# print(my_list*2)
+# my_list.append('this is new')
+# print(my_list)
+# my_list.pop(1)
+# print(my_list)
+# my_list.reverse()
+# print(my_list)
+
+# lst_1=[1,2,3]
+# lst_2=[4,5,6]
+# lst_3=[7,8,9]
+#
+# matrix = [lst_1,lst_2,lst_3]
+#
+# # print(matrix[1][1:])
+#
+# first_col = [row[0:2] for row in matrix]
+#
+# print(first_col)
+
+# d = {'key1':{'nestkey':{'subnestkey':'value'}}}
+# print(d['key1']['nestkey']['subnestkey'])
+
+# t = ('one', 2, 'one', 1, 3, 'one')
+# # print(t.count('one'))
+# # t[0]= 'change'
+# t.append('nope')
+
+l = [1,1,2,2,3,4,5,6,1,1]
+
+print(set(l))
